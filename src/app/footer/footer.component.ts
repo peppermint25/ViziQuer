ba408bbbebf487252990d0cba09763484912e1fb
peppermint25 <oskars.metr01@gmail.com>
2023-06-02 12:32:04 +0300

    Modified scrollbar Added information to documentation component Unified separate components

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  current_year = new Date().getFullYear();
+
 }
--- src/app/querry-environment/querry-environment.component.ts
+++ src/app/querry-environment/querry-environment.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
+  selector: 'app-querry-environment',
   templateUrl: './querry-environment.component.html',
   styleUrls: ['./querry-environment.component.scss']
 })
-export class QuerryEnvironmentComponent {
+export class QuerryEnvironmentComponent implements OnInit, OnDestroy {
+  iframeSrc: SafeResourceUrl | null = null;
 
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    const storedSrc = sessionStorage.getItem('iframeSrc');
+    if (storedSrc) {
+      this.iframeSrc = this.sanitizer.bypassSecurityTrustResourceUrl(storedSrc);
+    }
+  }
+
+  ngOnDestroy(): void {
+    sessionStorage.removeItem('iframeSrc');
+  }
+
+  saveIframeSrc(): void {
+    const iframeElement = document.getElementById('app_iframe') as HTMLIFrameElement;
+    const iframeSrc = iframeElement.src;
+    sessionStorage.setItem('iframeSrc', iframeSrc);
+  }
 }
