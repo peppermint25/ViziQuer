dc8df243af901314deb8214d20cce61073a3deb2
peppermint25 <oskars.metr01@gmail.com>
2023-06-06 10:30:49 +0300

    More documentation

--- src/app/documentation/documentation.component.ts
+++ src/app/documentation/documentation.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+// import { ScrollService } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
+  selector: 'app-documentation',
   templateUrl: './documentation.component.html',
   styleUrls: ['./documentation.component.scss']
 })
 export class DocumentationComponent {
+
+  // scrollTo(sectionId: string) {
+  //   const element = document.getElementById(sectionId);
+  //   if (element) {
+  //     this.scrollService.scrollTo(element);
+  //   }
+  // }
 
 }
