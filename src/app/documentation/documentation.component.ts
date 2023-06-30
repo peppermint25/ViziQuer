077de0caac799fccdfb35879dc543385ca5cdd35
peppermint25 <oskars.metr01@gmail.com>
2023-06-30 12:31:01 +0300

    slight changes and scrollspy

--- src/app/documentation/documentation.component.ts
+++ src/app/documentation/documentation.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-// import { ScrollService } from '@ng-bootstrap/ng-bootstrap';
+import { Component, HostListener, ViewChild, ElementRef, AfterViewInit, OnInit, AfterViewChecked, Renderer2} from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+declare var $: any;
+declare var bootstrap: any;
 
 
 @Component({
@@ -7,6 +9,39 @@ import { Component } from '@angular/core';
   templateUrl: './documentation.component.html',
   styleUrls: ['./documentation.component.scss']
 })
-export class DocumentationComponent {
 
+export class DocumentationComponent implements AfterViewInit {
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngAfterViewInit(): void {
+    this.initializeScrollspy();
+
+    // Re-initialize ScrollSpy when route changes
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.reinitializeScrollspy();
+      }
+    });
+  }
+
+  private initializeScrollspy(): void {
+    let scrollSpy = bootstrap.ScrollSpy.getInstance(document.body);
+
+    if (!scrollSpy) {
+      scrollSpy = new bootstrap.ScrollSpy(document.body, {
+      target: '#navbar'
+      });
+    }
+  }
+
+  private reinitializeScrollspy(): void {
+    const scrollSpy = bootstrap.ScrollSpy.getInstance(document.body);
+
+    if (scrollSpy) {
+      scrollSpy.dispose();
+    }
+
+    this.initializeScrollspy();
+  }
 }
