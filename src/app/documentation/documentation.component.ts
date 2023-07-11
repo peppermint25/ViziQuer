df8d96ae24a09b63de57a0cc2471c3188d629ccb
peppermint25 <oskars.metr01@gmail.com>
2023-07-11 10:16:27 +0300

    slight home update

--- src/app/documentation/documentation.component.ts
+++ src/app/documentation/documentation.component.ts
@@ -10,53 +10,55 @@ declare const bootstrap: any;
   styleUrls: ['./documentation.component.scss']
 })
 
-// export class DocumentationComponent implements AfterViewInit {
+export class DocumentationComponent implements AfterViewInit {
 
-//   constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
-//   ngAfterViewInit(): void {
-//     this.initializeScrollspy();
+  ngAfterViewInit(): void {
+    this.initializeScrollspy();
 
-//     // Re-initialize ScrollSpy when route changes
-//     this.router.events.subscribe(event => {
-//       if (event instanceof NavigationEnd) {
-//         this.reinitializeScrollspy();
-//       }
-//     });
-//   }
-
-//   private initializeScrollspy(): void {
-//     let scrollSpy = bootstrap.ScrollSpy.getInstance(document.body);
-
-//     if (!scrollSpy) {
-//       scrollSpy = new bootstrap.ScrollSpy(document.body, {
-//       target: '#navbar'
-//       });
-//     }
-//   }
-
-//   private reinitializeScrollspy(): void {
-//     const scrollSpy = bootstrap.ScrollSpy.getInstance(document.body);
-
-//     if (scrollSpy) {
-//       scrollSpy.dispose();
-//     }
-
-//     this.initializeScrollspy();
-//   }
-// }
-
-export class DocumentationComponent implements AfterViewInit{
-
-  ngAfterViewInit() {
-    this.scrollspy();
+    // Re-initialize ScrollSpy when route changes
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.reinitializeScrollspy();
+      }
+    });
   }
 
-  scrollspy() {
-    const scrollspy = new bootstrap.ScrollSpy(document.body, {
-      target: '#navbar',
-      offset: 0
-    });
-    scrollspy.refresh(); // Refresh the ScrollSpy plugin after initialization
+  private initializeScrollspy(): void {
+    let scrollSpy = bootstrap.ScrollSpy.getInstance(document.body);
+
+    if (!scrollSpy) {
+      scrollSpy = new bootstrap.ScrollSpy(document.body, {
+      target: '#navbar'
+      });
+    }
+  }
+
+  private reinitializeScrollspy(): void {
+    const scrollSpy = bootstrap.ScrollSpy.getInstance(document.body);
+
+    if (scrollSpy) {
+      scrollSpy.dispose();
+    }
+
+    this.initializeScrollspy();
   }
 }
+
+
+
+// export class DocumentationComponent implements AfterViewInit{
+
+//   ngOnInit() {
+//     this.refreshScrollSpy();
+//   }
+
+//   refreshScrollSpy() {
+//     const scrollspy = new bootstrap.ScrollSpy(document.body, {
+//       target: '#navbar',
+//       offset: 0
+//     });
+//     scrollspy.refresh();
+//   }
+// }
